fdc6c65b4fdc59dcd11a0268e1fbb7e058654a23
Evan Thibodeau <evan.thibodeau@liferay.com>
2020-10-20 13:05:34 +0400

    Add Logic

--- modules/toggle-control-menu-personal-menu-entry/src/main/java/com/liferay/users/admin/web/internal/product/navigation/personal/menu/ToggleControlMenuPersonalMenuEntry.java
+++ modules/toggle-control-menu-personal-menu-entry/src/main/java/com/liferay/users/admin/web/internal/product/navigation/personal/menu/ToggleControlMenuPersonalMenuEntry.java
@@ -1,21 +1,44 @@
 package com.liferay.users.admin.web.internal.product.navigation.personal.menu;
 
-import PersonalMenuEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.SessionClicks;
+import com.liferay.product.navigation.personal.menu.PersonalMenuEntry;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
 
 /**
- * @author evanthibodeau
+ * @author Evan Thibodeau
  */
 @Component(
 	immediate = true,
 	property = {
-		// TODO enter required service properties
+		"product.navigation.personal.menu.entry.order:Integer=100",
+		"product.navigation.personal.menu.group:Integer=399"
 	},
 	service = PersonalMenuEntry.class
 )
 public class ToggleControlMenuPersonalMenuEntry implements PersonalMenuEntry {
 
-	// TODO enter required service methods
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "toggle-control-menu");
+	}
+
+	@Override
+	public String getPortletURL(HttpServletRequest httpServletRequest)
+		throws PortalException {
+		
+		boolean showControlMenu = GetterUtil.getBoolean(SessionClicks.get(httpServletRequest, "SHOW_CONTROL_MENU", "default"));
+
+		String showControlMenuNext = showControlMenu ? "false" : "true";
+
+		return "javascript: Liferay.Store('SHOW_CONTROL_MENU', '" + showControlMenuNext + "'); location.reload();";
+	}
 
 }
