3a227fe9eeadba9cea5ebcbe4f324c9bc153e04e
Evan Thibodeau <evan.thibodeau@liferay.com>
2020-10-20 10:43:54 +0400

    init service

---
+++ modules/toggle-control-menu-personal-menu-entry/src/main/java/com/liferay/users/admin/web/internal/product/navigation/personal/menu/ToggleControlMenuPersonalMenuEntry.java
@@ -0,0 +1,21 @@
+package com.liferay.users.admin.web.internal.product.navigation.personal.menu;
+
+import PersonalMenuEntry;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author evanthibodeau
+ */
+@Component(
+	immediate = true,
+	property = {
+		// TODO enter required service properties
+	},
+	service = PersonalMenuEntry.class
+)
+public class ToggleControlMenuPersonalMenuEntry implements PersonalMenuEntry {
+
+	// TODO enter required service methods
+
+}
